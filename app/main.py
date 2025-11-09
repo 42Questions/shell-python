cfe81ec5abc8823ede3bb244bccf33b293f050bc
42Questions <46352502+42Questions@users.noreply.github.com>
2025-11-09 17:23:37 +0000

    Enhance command execution with output handling
    Refactor execute_user_command to return output and handle redirection.

--- app/main.py
+++ app/main.py
@@ -1,40 +1,72 @@
 import os
+import shlex
 import sys
 from typing import Final
 import subprocess
 
-from .shell_command import ShellCommand
-from .utils import CommandKwargs, find_executable
-from .constants import Commands, ReturnCode
-from .config import COMMANDS
+from app.shell_command import ShellCommand
+from app.utils import CommandKwargs, find_executable
+from app.constants import Commands, ReturnCode
+from app.config import COMMANDS
 
-def execute_user_command(args: list[str], command_kwargs: CommandKwargs) -> ReturnCode:
+def execute_user_command(args: list[str], command_kwargs: CommandKwargs) -> tuple[ReturnCode, str|None]:
     try:
         if find_executable(args[0], command_kwargs.paths):
-            proc = subprocess.run([args[0], *args[1:]])
-            return ReturnCode.SUCCESS if proc.returncode == 0 else ReturnCode.FAILURE
+            proc = subprocess.run([args[0], *args[1:]], 
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                text=True)
+            return_code = ReturnCode.SUCCESS if proc.returncode == 0 else ReturnCode.FAILURE
+            output = proc.stdout if proc.stdout else proc.stderr
+            return return_code, output
         else:
-            sys.stdout.write(f"{args[0]}: command not found\n")
-            return ReturnCode.FAILURE
+            return ReturnCode.FAILURE, f"{args[0]}: command not found\n"
     except Exception as e:
-        sys.stdout.write(f"Error executing command: {e}\n")
-        return ReturnCode.FAILURE
+        return ReturnCode.FAILURE, f"Error executing command: {e}\n"
+
+def get_redirect_to(args: list[str]) -> tuple[list[str], str|None]:
+    new_args: list[list[str]] = []
+    has_redirect:bool = False
+    for arg in args:
+        if arg == ">" or arg == "1>":
+            has_redirect = True
+            break
+        new_args.append(arg)
+    if has_redirect:
+        redirect_to = args[len(args)-1]
+        return new_args, redirect_to
+    return args, None
 
 def main():
+    print(os.getcwd())
     running = True
     while running:
         sys.stdout.write("$ ")
-        args: Final[list[str]] = input().split(" ")
+        args, redirect_to = get_redirect_to(shlex.split(input(), posix=True))
         if args[0] == Commands.EXIT.value:
             return 0
         paths: Final[list[str]] = os.getenv("PATH","").split(os.pathsep)
         command_kwargs: Final[CommandKwargs] = CommandKwargs(paths=paths)
 
+        return_code, output = None, None
         if COMMANDS.get(args[0]):
             command: Final[ShellCommand] =COMMANDS[args[0]](args, command_kwargs)
-            command.execute()
+            return_code, output = command.execute()
         else:
-            execute_user_command(args, command_kwargs)
+            return_code, output = execute_user_command(args, command_kwargs)
+
+        if redirect_to:
+            try:
+                with open(redirect_to, "w") as f:
+                    if output:
+                        f.write(output)
+            except Exception as e:
+                sys.stderr.write(f"Error redirecting output to {redirect_to}: {e}\n")
+        else:
+            if return_code == ReturnCode.SUCCESS and output:
+                sys.stdout.write(output)
+            elif return_code == ReturnCode.FAILURE and output:
+                sys.stderr.write(output)
 
 if __name__ == "__main__":
     main()
